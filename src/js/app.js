d89531ba8b4eaa4aead931c3508de6cbb753bf41
Maksim K <6606745@gmail.com>
2024-09-11 01:35:19 +0300

    адаптивность

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+const navMain = document.querySelector(".main-navigation");
+const navToggle = document.querySelector(".main-navigation__toggle");
+navToggle.addEventListener("click", () => {
+  if (navMain.classList.contains("main-navigation--closed")) {
+    navMain.classList.remove("main-navigation--closed");
+    navMain.classList.add("main-navigation--opened");
+  } else {
+    navMain.classList.add("main-navigation--closed");
+    navMain.classList.remove("main-navigation--opened");
+  }
+});
